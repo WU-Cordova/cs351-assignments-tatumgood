015fe4feb1cf0e2ac2e12b38f8766a58fbf16f30
tatumgood <tagood@willamette.edu>
2024-10-02 09:59:18 -0700

    Update program.py

--- program.py
+++ program.py
@@ -1,16 +1,11 @@
-from tests.car import Car, Color, Make, Model
 
+from datastructures.avltree import AVLTree
 def main():
-    print('Hello world!')
-
-    car = Car(vin='123456789', color=Color.RED, make=Make.TOYOTA, model=Model.COROLLA)
-    print(car)
+    tree = AVLTree[int, int]()
+    for node in [8, 9, 10, 2, 1, 5, 3, 6, 4, 7]:
+        tree.insert(node, node)
+    
+    print(tree.inorder())
 
 if __name__ == '__main__':
     main()
-
-
-
-from datastructures.avl import AVLTree
-def main():
-    tree = AVLTree[int,int]
