d1356e9d314ed49325bd447719f0c9d494dc5651
tatumgood <tagood@willamette.edu>
2024-10-04 11:50:36 -0700

    Update program.py

--- program.py
+++ program.py
@@ -1,22 +1,25 @@
 
 from datastructures.avltree import AVLTree
 def main():
-    tree = AVLTree[int, int]()
-    for node in [8, 9, 10, 2]:
-        tree.insert(node, node)
+    # tree = AVLTree[int, int]()
+    # for node in [8, 9, 10, 2]:
+    #     tree.insert(node, node)
     
-    print(tree.bforder())
+    # print(tree.bforder())
 
-    def print_node(value: int) -> None:
-        print(value)
+    # def print_node(value: int) -> None:
+    #     print(value)
 
-    #using a built in higher order func
-    _ = tree.bforder(print)
-    _ = tree.bforder(print_node)
+    # #using a built in higher order func
+    # _ = tree.bforder(print)
+    # _ = tree.bforder(print_node)
 
-    _ = tree.bforder(lambda value: print(value))
+    # _ = tree.bforder(lambda value: print(value))
 
-    
+    avltree = AVLTree()
+    avltree.insert(5)
+
+    print(avltree)
 
 
 if __name__ == '__main__':
